6015a0890f6da2182295da9c41804414e1ff3e68
Natalia Vargas <natalia.romina.vargas@gmail.com>
2023-04-26 01:36:43 +0000

    Agrego registros de la cpu en el contexto de ejec

--- static/src/utils/estructuras.h
+++ static/src/utils/estructuras.h
@@ -13,7 +13,18 @@
 typedef struct{
 	t_list* instrucciones;
 	uint32_t PC;
-	t_list registrosCPU; //Lo dejo con list porque no sé todavía la estructura
+	    char AX[4];
+	    char BX[4];
+	    char CX[4];
+	    char DX[4];
+	    char EAX[8];
+	    char EBX[8];
+	    char ECX[8];
+	    char EDX[8];
+	    char RAX[16];
+	    char RBX[16];
+	    char RCX[16];
+	    char RDX[16];
 }contextoEjec;
 
 typedef struct{
