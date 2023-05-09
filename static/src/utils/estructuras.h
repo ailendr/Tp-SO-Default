351116589d0d0a61f26718eaaab8595acc2bf50b
Dany Mereles <mdmereles30@gmail.com>
2023-05-09 04:26:21 +0000

    2do intento: Nuevas estructuras

--- static/src/utils/estructuras.h
+++ static/src/utils/estructuras.h
@@ -12,23 +12,30 @@
 #include <commons/collections/list.h>
 
 typedef struct{
-	    t_list* instrucciones;
-	    uint32_t PC;
-	    char AX[4];
-	    char BX[4];
-	    char CX[4];
-	    char DX[4];
-	    char EAX[8];
-	    char EBX[8];
-	    char ECX[8];
-	    char EDX[8];
-	    char RAX[16];
-	    char RBX[16];
-	    char RCX[16];
-	    char RDX[16];
-
-	uint32_t proxInstruccion;
+	uint32_t pid;
+	t_list* instrucciones;
+	uint32_t PC;
+	char AX[4];
+	char BX[4];
+    char CX[4];
+    char DX[4];
+	char EAX[8];
+    char EBX[8];
+	char ECX[8];
+    char EDX[8];
+	char RAX[16];
+    char RBX[16];
+	char RCX[16];
+    char RDX[16];
 }t_contextoEjec;
+
+typedef struct {
+	char* nombre;
+	uint32_t pid;
+	char* param1;
+	char* param2;
+	char* param3;
+}t_instruccion;
 
 typedef enum{
 	NEW,
