1e296d0fb4a0d3c5df6426942334bd255c869481
Danymmereles <mdmereles30@gmail.com>
2023-06-17 21:06:04 +0000

    nuevos inicios

---
+++ ConsolaT/src/consola.h
@@ -0,0 +1,19 @@
+/*
+ * consola.h
+ *
+ *  Created on: Apr 16, 2023
+ *      Author: utnso
+ */
+
+#ifndef SRC_CONSOLA_H_
+#define SRC_CONSOLA_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <utils/sockets.h>
+#include <utils/modulo.h>
+#include "ConsolaConfig.h"
+
+//Para mostrar el git add /ConsolaT
+
+#endif /* SRC_CONSOLA_H_ */
