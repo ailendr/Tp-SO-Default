65195ad8e563ffea586b4d7e4e327f9cfed566bc
Ailen Da Ruos <ailendr2812@gmail.com>
2023-07-29 00:41:29 +0000

    Vaciando main

--- CpuT/src/cpu.h
+++
@@ -1,9 +0,0 @@
-#ifndef SRC_CPU_H_
-#define SRC_CPU_H_
-
-#include <stdlib.h>
-#include <stdio.h>
-
-#include "./instruccion.h"
-
-#endif
--- static/src/utils/modulo.h
+++ static/src/utils/modulo.h
@@ -1,19 +1,1 @@
-/*
- * modulo.h
- *
- *  Created on: Apr 16, 2023
- *      Author: utnso
- */
 
-#ifndef SRC_UTILS_MODULO_H_
-#define SRC_UTILS_MODULO_H_
-
-#include <commons/log.h>
-#include <commons/config.h>
-#include "sockets.h"
-
-int verificarSocket (int socket, t_log* logger, t_config* config);
-int verificarConfig (int socket, t_log* logger, t_config* config);
-
-
-#endif /* SRC_UTILS_MODULO_H_ */
